78fcfd2b00e2eebff6cf3b1d34422584e72669c2
Theo <theobbb64@gmail.com>
2025-10-20 18:29:28 -0400

    s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <div
-	class="relative grid h-svh grid-cols-6 grid-rows-[auto_auto_auto_1fr] gap-x-6 gap-y-12 overflow-hidden px-3 py-2 text-xl/5.5 text-balance text-yellow lg:px-12 lg:py-6 lg:text-3xl/8"
+	class="relative grid h-svh grid-cols-6 grid-rows-[auto_auto_auto_1fr] gap-x-6 gap-y-12 overflow-hidden px-3 py-2 text-lg/5 text-yellow lg:px-12 lg:py-6 lg:text-3xl/8"
 >
 	<div class="col-span-3 text-sm/4 max-lg:col-start-4 lg:text-lg/5.5">
 		<div class="flex max-lg:justify-end">*Site web en construction*</div>
@@ -15,8 +15,8 @@
 	</div>
 
 	<div class="col-span-full lg:col-span-4 lg:-mt-28">
-		<div class="border-l-3 border-orange pl-4 text-orange">
-			Design, conception & développement <br /> d’expériences numériques uniques &<br /> performantes
+		<div class="max-w-100 border-l-3 border-orange pl-4 text-orange lg:max-w-200">
+			Design, conception & développement d’expériences numériques uniques & performantes
 		</div>
 
 		<div class="mt-24">
@@ -37,16 +37,14 @@
 			</a>
 		</div>
 	</div>
-
-	<!-- <div class="col-span-full text-[10vw]/[10vw] text-pink">tripledoubleve</div> -->
 </div>
-<div class="fixed right-12 bottom-0 left-0 h-[50svh]">
+<div class="fixed right-12 bottom-0 left-0 h-[50svh] min-h-135">
 	<svg
 		width="100vw"
 		height="100svh"
 		viewBox="0 0 86 55"
 		fill="none"
-		class="absolute top-0 left-0 origin-top-right scale-105"
+		class="absolute top-0 left-0 origin-top-right scale-110"
 		xmlns="http://www.w3.org/2000/svg"
 	>
 		<path d="M49 2V7H84V41H29V2H49Z" fill="#719043" stroke="#B93D1E" stroke-width="4" />
@@ -54,24 +52,3 @@
 		<path d="M22 14V19H57V53H2V14H22Z" fill="#C86228" stroke="#392C5E" stroke-width="4" />
 	</svg>
 </div>
-<!-- <div class="absolute grid h-full w-full grid-cols-3">
-			{#each folders as folder, i}
-				<svg
-					width="100%"
-					height="100%"
-					viewBox="0 0 {viewbox.w} {viewbox.h}"
-					fill="none"
-					class="relative origin-top-right"
-					style="transform: scale(1.5) translateY({(folders.length - 1 - i) * 15}%) translateX({i *
-						-25 +
-						17}%); z-index: {folders.length - i}"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						d="M{w} {h} V{top}H{left}L{left} {s}H{s}L{s} {top}V{h}H{w}Z"
-						class={folder}
-						stroke-width={stroke_width}
-					/>
-				</svg>
-			{/each}
-		</div> -->
