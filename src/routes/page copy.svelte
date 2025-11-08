5053b58ded8990dced43a7589fbab8a98db45dc9
Theo <theobbb64@gmail.com>
2025-11-08 11:54:23 -0500

    d

---
+++ src/routes/page copy.svelte
@@ -0,0 +1,60 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>3xw - tripledoubleve</title>
+	<meta
+		name="description"
+		content="Design, conception & développement d’expériences numériques uniques & performantes"
+	/>
+</svelte:head>
+
+<div
+	class="relative grid h-svh grid-cols-6 grid-rows-[auto_auto_auto_1fr] gap-x-6 gap-y-12 overflow-hidden px-3 py-2 text-lg/5 text-yellow lg:px-12 lg:py-6 lg:text-3xl/8"
+>
+	<div class="col-span-3 text-sm/4 max-lg:col-start-4 lg:text-lg/5.5">
+		<div class="flex max-lg:justify-end">*Site web en construction*</div>
+	</div>
+	<div class="col-span-3 flex text-pink max-lg:col-start-1 max-lg:row-start-1 lg:justify-end">
+		<div class="items-center">
+			<div class="text-[5.9rem]/[4.5rem] lg:text-[9rem]/[7rem]">3xw</div>
+			<div>tripledoublevé</div>
+		</div>
+	</div>
+
+	<div class="col-span-full lg:col-span-4 lg:-mt-28">
+		<div class="max-w-100 border-l-3 border-orange pl-4 text-orange lg:max-w-200">
+			Design, conception & développement d’expériences numériques uniques & performantes
+		</div>
+
+		<div class="mt-24">
+			<a href="mailto:info@3xw.ca" class="flex w-fit items-center gap-1 hover:border-b-3">
+				<span class="mr-3">
+					<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
+						><path
+							fill="currentColor"
+							d="M20 4H2v16h10v-2H4V6h16v6h2V4zM6 8h2v2H6zm4 4H8v-2h2zm4 0v2h-4v-2zm2-2v2h-2v-2zm0 0V8h2v2zm8 8h-2v-2h-2v-2h-2v2h2v2h-6v2h6v2h-2v2h2v-2h2v-2h2z"
+						/></svg
+					>
+				</span>
+				info
+				<span class="text-5xl">@</span>
+				3xw.ca
+			</a>
+		</div>
+	</div>
+</div>
+<div class="pointer-events-none fixed right-12 bottom-0 left-0 h-[50svh] min-h-135">
+	<svg
+		width="100vw"
+		height="100svh"
+		viewBox="0 0 86 55"
+		fill="none"
+		class="absolute top-0 left-0 origin-top-right scale-110"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path d="M49 2V7H84V41H29V2H49Z" fill="#719043" stroke="#B93D1E" stroke-width="4" />
+		<path d="M34 8V13H69V47H14V8H34Z" fill="#D391A2" stroke="#F4D641" stroke-width="4" />
+		<path d="M22 14V19H57V53H2V14H22Z" fill="#C86228" stroke="#392C5E" stroke-width="4" />
+	</svg>
+</div>
