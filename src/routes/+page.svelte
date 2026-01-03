47ecef25aff29b6347e267eb1bc48605b3c2e8e5
Theo <theobbb64@gmail.com>
2026-01-03 16:14:58 -0500

    g

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,23 +6,28 @@
 	/>
 </svelte:head>
 
-<div class="mx-4 my-2.5">
-	<div class="text-2xl/7 text-balance">Design, conception & développement de sites web</div>
-	<div class="mt-24 ml-12 flex items-center">
-		<span class="mr-3">
-			<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
-				><path
-					fill="currentColor"
-					d="M20 4H2v16h10v-2H4V6h16v6h2V4zM6 8h2v2H6zm4 4H8v-2h2zm4 0v2h-4v-2zm2-2v2h-2v-2zm0 0V8h2v2zm8 8h-2v-2h-2v-2h-2v2h2v2h-6v2h6v2h-2v2h2v-2h2v-2h2z"
-				/></svg
-			>
-		</span>
+<div class="mx-4 my-2 grid grid-cols-12 text-2xl/8 lg:mx-9 lg:my-6">
+	<div class="col-span-full max-sm:text-balance lg:col-span-7">
+		<div class="max-w-130">Design, conception & développement de services web</div>
+	</div>
 
-		<div>
-			<a
-				class="text-lg decoration-2 underline-offset-1 hover:underline"
-				href="mailto:theobbb64@gmail.com">theobbb64@gmail.com</a
-			>
+	<div class="col-span-full max-lg:mt-16 lg:col-span-4">
+		<div class="inline-flex">
+			<span class="mr-3">
+				<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
+					><path
+						fill="currentColor"
+						d="M20 4H2v16h10v-2H4V6h16v6h2V4zM6 8h2v2H6zm4 4H8v-2h2zm4 0v2h-4v-2zm2-2v2h-2v-2zm0 0V8h2v2zm8 8h-2v-2h-2v-2h-2v2h2v2h-6v2h6v2h-2v2h2v-2h2v-2h2z"
+					/></svg
+				>
+			</span>
+
+			<div>
+				<a class="decoration-2 underline-offset-1 hover:underline" href="mailto:theobbb64@gmail.com"
+					>theobbb64@gmail.com</a
+				>
+			</div>
 		</div>
+		<!-- <div>(adresse professionnelle)</div> -->
 	</div>
 </div>
